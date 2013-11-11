8314efccbb0cd654f01e82f3101c9d4db7a347e7
Paul <beriukay+github@gmail.com>
2013-11-11 06:18:44 -0900

    cleaned up code

--- spline_f.glsl
+++ spline_f.glsl
@@ -14,33 +14,23 @@
  */
 
 uniform float myf1;        // Application float (in [0.,1.]; start: 1.)
-uniform bool myb1;         // Application bool (start: true)
+uniform bool myb1;         // Application bool (turn on/off shader)
 
 varying vec3 surfpt;       // Point on surface (camera coords)
 varying vec3 surfnorm_un;  // Surface normal (camera coords)
-                           // Normalize before & after sending
-
 
 // bpLight
 // Blinn-Phong illumination model
 // Given light-source color & position/direction, surface paint color,
 // position, & normal vec (normalized). Return apparent surface color.
-vec4 bpLight(
-    vec4 lightcolor,
-    vec4 lightpos4,  // Homogeneous form
-    vec4 paintcolor,
-    vec3 surfpt,
-    vec3 surfnorm)   // Normalized
+vec4 bpLight( vec4 lightcolor, vec4 lightpos4,  // Homogeneous form
+              vec4 paintcolor, vec3 surfpt, vec3 surfnorm)   
 {
     // Compute normalized direction of light source from object
     vec3 lightdir = normalize(lightpos4.xyz - surfpt*lightpos4.w);
-        // Odd computation above is correct for both positional and
-        //  directional lights.
 
-    // Scalar lighting parameters
-    float ambientfraction = 0.2;
-        // Ambient light color, as fraction of light color
-    float shininess = 5. + myf1 * 70.;
+    float ambientfraction = 0.2; // Scalar lighting parameter
+    float shininess = 5. + myf1 * 70.; // Ambient light fraction of light color
 
     // Ambient
     vec4 ambientcolor = ambientfraction * lightcolor * paintcolor;
@@ -54,15 +44,11 @@ vec4 bpLight(
     vec3 halfway = normalize(viewdir + lightdir);
     float specularcoeff = pow(max(0., dot(surfnorm, halfway)),
                               4.*shininess);
-        // Blinn-Phong needs shininiess about 4 * Phong shininess
-    vec4 specularcolor = specularcoeff * lightcolor;
+    vec4 specularcolor = myb1? specularcoeff * lightcolor : vec4(0.,0.,0.,1.);
 
     // Compute final color
-    return clamp(ambientcolor + diffusecolor
-                   + (myb1 ? specularcolor : vec4(0.,0.,0.,1.)),
-                 0., 1.);  // Clamp to [0,1]
+    return clamp(ambientcolor + diffusecolor + specularcolor, 0., 1.);  
 }
-
 
 void main()
 {
@@ -79,7 +65,6 @@ void main()
     vec4 paintcolor = gl_Color;
 
     // Apply Blinn-Phong illumination model
-    gl_FragColor = bpLight(lightcolor, lightpos4,
-                           paintcolor, surfpt, surfnorm);
+    gl_FragColor = bpLight(lightcolor, lightpos4, paintcolor, surfpt, surfnorm);
 }
 
